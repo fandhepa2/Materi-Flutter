e0e4d2341132d86b4d982ce3bcd70f4fab013caa
fandhepa2 <fandhepaharahap7319@gmail.com>
2023-03-18 14:03:58 +0700

    materi 18 maret

--- materi6/1.1syncronous.dart
+++ materi6/1.1syncronous.dart
@@ -1,17 +1,14 @@
-void main(){
+void main() {
   // syncronous => menjalankan script berdasarkan urutan dari baris ke 1 dan ke n...
   // membuat object
   Syncronous1 syncronous1 = new Syncronous1();
   print("Proses data...");
   syncronous1.fetchData();
   print("Data Success Loaded!");
-  
 }
 
 class Syncronous1 {
-
-  void fetchData(){
+  void fetchData() {
     print("Fetch data..");
   }
-
 }
